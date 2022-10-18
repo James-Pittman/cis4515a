c7aad4ee508e99e0a258a8e71d7cb36f5c828ad2
James-Pittman <jamsoaf@knights.ucf.edu>
2022-10-17 21:52:53 -0400

    Selected vulnerabilities non-compliant

---
+++ R08_VNA01_J.java
@@ -0,0 +1,21 @@
+// Immutable Helper
+public final class Helper {
+  private final int n;
+ 
+  public Helper(int n) {
+    this.n = n;
+  }
+  // ...
+}
+
+final class Foo {
+  private Helper helper;
+ 
+  public Helper getHelper() {
+    return helper;
+  }
+ 
+  public void setHelper(int num) {
+    helper = new Helper(num);
+  }
+}
